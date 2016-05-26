a5e681fa2044e15ec1bc0dd6466e07bbe6fda5c3
Clint Geense <clint_geense@hotmail.com>
2016-05-26 14:34:59 +0200

    files

---
+++ PostorderCompany/PostorderCompany.Magazijn/MagazijnService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Nancy.ViewEngines;
+using Newtonsoft.Json;
+using PostorderCompany.Core.Events;
+using PostorderCompany.Core.Infrastructure;
+using PostorderCompany.Magazijn.Model;
+
+namespace PostorderCompany.Magazijn
+{
+    public class MagazijnService
+    {
+
+        public static List<OrderIngepakt> orders = new List<OrderIngepakt>();
+
+
+        public MagazijnService() {
+            var eventHandler = new RabbitMQEventHandler("PostorderCompany.Magazijn", HandleEvent);
+            eventHandler.Start();
+
+            orders.Add(new OrderIngepakt() {
+                afmetingen =  null,
+                gewicht = null,
+                orderId = "123456",
+                routingKey = "15"
+            });
+
+            orders.Add(new OrderIngepakt() {
+                afmetingen = null,
+                gewicht = null,
+                orderId = "654321",
+                routingKey = "15"
+            });
+
+            orders.Add(new OrderIngepakt() {
+                afmetingen = null,
+                gewicht = null,
+                orderId = "555555",
+                routingKey = "15"
+            });
+        }
+
+        public void sendOrder(OrderIngepakt order) {
+            new RabbitMQEventPublisher().PublishEvent(order);
+            orders.Remove(order);
+        }
+
+        private static bool HandleEvent(string eventType, string eventData) {
+            bool handled = true;
+            switch (eventType) {
+                case "OrderOntvangen":
+                    OrderOntvangen orderOntvangen = JsonConvert.DeserializeObject<OrderOntvangen>(eventData);
+                    handled = Handle(orderOntvangen);
+                    break;
+            }
+            return handled;
+        }
+
+        private static bool Handle(OrderOntvangen orderOntvangen) {
+
+            var order = new OrderIngepakt() {
+                routingKey = "Order.Ingepakt",
+                orderId = orderOntvangen.orderId,
+                afmetingen = null,
+                gewicht = null,
+            };
+
+            orders.Add(order);
+
+            return true;
+        }
+
+        public List<OrderIngepakt> GetOrders() {
+            return orders;
+        }
+    }
+}
---
+++ PostorderCompany/PostorderCompany.Magazijn/Model/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace PostorderCompany.Magazijn.Model {
+
+    public class Order {
+        public string OrderId { get; set; }
+        public string Afmetingen { get; set; }
+        public string Gewicht { get; set; }
+        public bool Ingepakt { get; set; }
+    }
+}
